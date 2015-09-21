bd73cecffc0534ef64fe7582e0136e5355cd452f
Emmanuel Natividad <emmanuel@natividad.info>
2015-09-21 14:03:06 +0800

    Fix id for _register.php

--- sections/_register.php
+++ sections/_register.php
@@ -1,4 +1,4 @@
-<section id="faq" class="row">
+<section id="register" class="row">
   <div class="col-md-12">
     <h2>Sign-up to Participate</h2>
     <p>Lorem ipsum dolor sit amet consectetur adipiscing ilit.</p>
