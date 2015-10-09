7ad9f5fb03e8081fa77aa1440862d7bcd5b11191
TheJackalOfAllTrades <JDBRC.28@gmail.com>
2015-10-09 21:59:25 +0800

    Complete code with FAQs content

--- sections/_faq.php
+++ sections/_faq.php
@@ -1,6 +1,66 @@
 <section id="faq"><div class="container">
   <div class="col-md-12">
-    <h1>Frequently Asked Questions</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipiscing ilit.</p>
-  </div>
-</div></section>
+    <h1>FREQUENTLY ASKED QUESTIONS</h1>
+    <div class="row">
+      <div class="col-lg-6 col-md-6 col-s-6 col-xs-12">
+        <ul class="collapsible" data-collapsible="accordion">
+          <li>
+            <div class="collapsible-header">Who can join IMC: Optima?</div>
+            <div class="collapsible-body"><p class="flow-text">Optima is open to undergraduate students of all universities within the metropolitan area. You don’t have to be an IT major to apply!</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header">What do I need to do in order to register?</div>
+            <div class="collapsible-body"><p class="flow-text">Fill up the application form and wait for the confirmation e-mail. Follow the steps indicated in the confirmation email to complete the registration.</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header">How much is the registration fee?</div>
+            <div class="collapsible-body"><p class="flow-text">The registration fee is at P400 per participant. However, participants who register within the first week of the registration period will pay only P370. For those who wish to register as a group of 5 members, the group rate will be P1900.</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header">How can I pay the registration fee?</div>
+            <div class="collapsible-body"><p class="flow-text">Payments can be made through bank deposit or through a point person from the team. Further details regarding payments will be included after the release of the results of accepted applicants.</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header">Can I pay on the day itself?</div>
+            <div class="collapsible-body"><p class="flow-text">No, you have to pay within 5 banking days after the release of the results of accepted applicants.</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header">Will refunds be allowed?</div>
+            <div class="collapsible-body"><p class="flow-text">Unfortunately, we do not allow refunds. However, we will allow you to have someone else (i.e. a friend, classmate) to take your slot. Don’t forget to notify us before the event of the changes!</p></div>
+          </li>
+        </ul>
+
+      </div>
+         <!-- Other side, last 5 questions!-->
+
+        <div class="col-lg-6 col-md-6 col-s-6 col-xs-12">
+         <ul class="collapsible" data-collapsible="accordion">
+          <li>
+            <div class="collapsible-header">What do I need to bring to the event?</div>
+            <div class="collapsible-body"><p class="flow-text">You don’t have to bring anything except for the deposit slip (for those who paid through bank deposit) as proof of payment for the application fee. Food will be provided for the duration of the event.</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header">What should I wear during the event?</div>
+            <div class="collapsible-body"><p class="flow-text">Participants should wear corporate/business attire to the event.</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header">What if I cannot attend the seminar (Day 1)?</div>
+            <div class="collapsible-body"><p class="flow-text">If you cannot attend the seminar (Day 1), then you will not be eligible to join the competition of Day 2.</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header">Are all participants required to join the competition?</div>
+            <div class="collapsible-body"><p class="flow-text">No, but we highly recommend all participants to join the competition. Big prizes await you!</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header">Can we form our own groups before the event?</div>
+            <div class="collapsible-body"><p class="flow-text">Yes, you are allowed to form 5-member teams prior to the event itself.</p></div>
+          </li>
+          <li>
+            <div class="collapsible-header">How many members per team is allowed for the competition?</div>
+            <div class="collapsible-body"><p class="flow-text">Each team is allowed at most 5 members. You can also participate in the competition alone.</p></div>
+          </li>
+        </ul>
+
+</div>
+</div>
+</section>
